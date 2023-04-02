f3ad21c40d80704b3ce68961f659b529949ff31b
Abrar Mahi <amahi2001@gmail.com>
2023-04-02 19:05:08 -0400

    revamped login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,10 +6,10 @@ Note: You don't need to work on this file for the Assignment.
 ==================================================*/
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import {Link} from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 
 class LogIn extends Component {
-  constructor (props) {  // Create and initialize state
+  constructor(props) {  // Create and initialize state
     super(props)
     this.state = {
       user: {
@@ -23,42 +23,40 @@ class LogIn extends Component {
 
   // When User Name input is changed, capture the new input value and update state
   handleChange = (e) => {
-    const updatedUser = {...this.state.user};  // Create an object for state
+    const updatedUser = { ...this.state.user };  // Create an object for state
     updatedUser.userName = e.target.value;  // Set object's userName to the new input value
-    this.setState({user: updatedUser})  // Update state with object values
+    this.setState({ user: updatedUser })  // Update state with object values
   }
 
   // When user clicked "Log In" button, store user data and then redirect to "User Profile" page
   handleSubmit = (e) => {
     e.preventDefault()
     this.props.mockLogIn(this.state.user)  // Update state in the top-level component (App.js)
-    this.setState({redirect: true})  // Update state to trigger Redirect
+    this.setState({ redirect: true })  // Update state to trigger Redirect
   }
-  
-  render () {
+
+  render() {
     // Redirect to "User Profile" page when "Log In" button is clicked
-    if (this.state.redirect) {  
-      return (<Redirect to="/userProfile"/>);
+    if (this.state.redirect) {
+      return (<Redirect to="/userProfile" />);
     }
 
     // Render the login form (and call "handleSubmit" method when "Log In" button is clicked to submit form)
     return (
       <div>
-        <h1>Login</h1>
-        
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>User Name</label>
-            <input type="text" name="userName" defaultValue={this.props.user.userName} onChange={this.handleChange} />
-          </div>
-          <div>
-            <label>Password</label>
-            <input type="password" name="password" />
-          </div>
-          <button>Log In</button>
-        </form>  
-        <br/>
-        <Link to="/">Return to Home</Link>
+        <h1 className='display-5 text-center mt-2'>Login</h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control required type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control required type="password" placeholder="Password" />
+          </Form.Group>
+          <button className='btn btn-outline-primary rounded-pill' type="submit">Log In</button>
+        </Form>
       </div>
     );
   }
