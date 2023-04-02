0942c69d46287443a2d6c5b0c682f346a054f55c
Abrar Mahi <amahi2001@gmail.com>
2023-04-02 16:35:51 -0400

    added navbar and corresponding styling globally

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ This is the top-level component of the app.
 It contains the top-level state.
 ==================================================*/
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
+//react bootstrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 // Import other components
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
@@ -57,13 +62,24 @@ class App extends Component {
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
       <Router basename="/WebDev-Assn-4">
-        <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand as={Link} to="/">Bank of React</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/credits">Credits</Nav.Link>
+              <Nav.Link as={Link} to="/debits">Debits</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Container>
           <Route exact path="/" render={HomeComponent} />
           <Route exact path="/userProfile" render={UserProfileComponent} />
           <Route exact path="/login" render={LogInComponent} />
           <Route exact path="/credits" render={CreditsComponent} />
           <Route exact path="/debits" render={DebitsComponent} />
-        </div>
+        </Container>
       </Router>
     );
   }
